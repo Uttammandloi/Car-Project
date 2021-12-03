6cc295df0fcd38a7b261e75fe08d3c4b56e10cc5
Uttam Mandloi <uttammandloi111@gmail.com>
2021-12-03 19:10:51 +0530

    commit and add the about and services,contact.

--- crazycars/testapp/urls.py
+++ crazycars/testapp/urls.py
@@ -18,4 +18,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("about/", views.about, name='about'),
+    path("service/", views.services, name='service'),
+    path("contact/", views.contact, name='contacts'),
 ]
