49a323249f591bf08c73c63f03348e7d47cff465
Uttam Mandloi <uttammandloi111@gmail.com>
2021-12-09 13:56:54 +0530

    commit cars app is created

--- crazycars/crazycars/settings.py
+++ crazycars/crazycars/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'testapp.apps.TestappConfig',
+    'cars.apps.CarsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
